132f1c27241d7e1ebffb0a0f173334563a3d7ac5
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2024-07-09 08:44:57 +0330

    minikube.sh

---
+++ Kuberneties/Minikube/minikube.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+set -e
+
+### step1 ) prerequisites
+
+# install requirement
+echo "-------------Installing requirement-------------"
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+# Set hostname
+echo "-------------Setting hostname-------------"
+hostnamectl set-hostname Minikube
+
+
+# Disable swap
+echo "-------------Disabling swap-------------"
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+swapon --show
+
+# Forwarding IPv4 and letting iptables see bridged traffic
+echo "-------------Setting IPTables-------------"
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+
+### step2) Install Docker
+#https://docs.docker.com/engine/install/ubuntu/
+echo "-------------Installing docker-------------"
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#verify :
+sudo docker run hello-world
+   
+### Step 3) Install kubectl 
+echo "-------------Installing Kubectl-------------"
+#https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#verify:
+kubectl version 
+kubectl get namespace 
+kubectl get nods
+kubectl get pods
+
+### Step 4) Install Minikube 
+echo "-------------Installing Mikukube-------------"
+#https://minikube.sigs.k8s.io/docs/start/?arch=%2Flinux%2Fx86-64%2Fstable%2Fbinary+download
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube 
+
+
+### Step 5) Start your cluster 
+minikube start --driver=docker 
+
+#verify: 
+minikube version 
+minikube status 
+minikube service list 
+kubectl get pv 
+minikube kubectl -- get nodes 
+   
+### Step 6) Enable addons 
+minikube addons enable ingress 
+minikube addons list 
