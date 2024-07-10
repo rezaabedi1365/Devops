2c429f28972d9a904ffd826488818fcfc00d9d5a
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2024-07-10 17:54:12 +0330

    Update master2.sh

--- Kuberneties/Kubeadm/master2.sh
+++ Kuberneties/Kubeadm/master2.sh
@@ -5,12 +5,10 @@
 #i1) Switch to root user [ sudo -i]
 
 #2) Disable swap & add kernel settings
-
 swapoff -a
 sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 #3) Add  kernel settings & Enable IP tables(CNI Prerequisites)
-
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
@@ -29,7 +27,6 @@ sysctl --system
 
 #4) Install containerd run time
 #To install containerd, first install its dependencies.
-
 apt-get update -y
 apt-get install ca-certificates curl gnupg lsb-release -y
 
@@ -39,58 +36,51 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 #Use follwing command to set up the repository:
-
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install containerd
-
 apt-get update -y
 apt-get install containerd.io -y
 
 # Generate default configuration file for containerd
-
 #Note: Containerd uses a configuration file located in /etc/containerd/config.toml for specifying daemon level options.
 #The default configuration can be generated via below command.
-
 containerd config default > /etc/containerd/config.toml
 
 # Run following command to update configure cgroup as systemd for contianerd.
-
 sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 
 # Restart and enable containerd service
-
 systemctl restart containerd
 systemctl enable containerd
 
 #5) Installing kubeadm, kubelet and kubectl
-
 # Update the apt package index and install packages needed to use the Kubernetes apt repository:
-
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl
 
 # Download the Google Cloud public signing key:
-
 curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 
 # Add the Kubernetes apt repository:
-
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Update apt package index, install kubelet, kubeadm and kubectl, and pin their version:
-
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 
 # apt-mark hold will prevent the package from being automatically upgraded or removed.
-
 apt-mark hold kubelet kubeadm kubectl
 
 # Enable and start kubelet service
-
 systemctl daemon-reload
 systemctl start kubelet
 systemctl enable kubelet.service
+
+#6) Initialize the control plane in the master node as the root user
+kubeadm init
+
+
+
