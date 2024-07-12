45f072549e29358f91051482370b867b06b97085
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2024-07-12 16:20:38 +0330

    Delete Kuberneties/Kubeadm/kubeconfig.sh

--- Kuberneties/Kubeadm/kubeconfig.sh
+++
@@ -1,8 +0,0 @@
-#7) Set up kubeconfig as normal ubuntu user
-#exit root as normal user
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-#to verify, if kubectl is working or not, run the following command.
-kubectl get pod -A
