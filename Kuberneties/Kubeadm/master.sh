5cbff5ef06634b38824ffbaf2fbf7fe890bd14e5
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2024-07-13 14:59:01 +0330

    Update master.sh

--- Kuberneties/Kubeadm/master.sh
+++ Kuberneties/Kubeadm/master.sh
@@ -5,7 +5,7 @@ set -e
 
 # Set hostname
 echo "-------------Setting hostname-------------"
-hostnamectl set-hostname master
+sudo hostnamectl set-hostname master
 #set /etc/hosts file and vrify it from ping 
 
 echo "-------------Install Requirement-------------"
@@ -13,14 +13,14 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 
 # Disable swap
 echo "-------------Disabling swap-------------"
-swapoff -a
-sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-swapon --show
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapon --show
 
 
 # Forwarding IPv4 and letting iptables see bridged traffic
 echo "-------------Setting IPTables-------------"
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+sudo cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
@@ -28,13 +28,13 @@ EOF
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
 EOF
 
-sysctl --system
+sudo sysctl --system
 
 ### step2) Install Containerd
 echo "-------------Installing Containerd-------------"
@@ -51,8 +51,8 @@ sudo systemctl restart containerd
 
 ### Step3) Install kubectl, kubelet and kubeadm
 echo "-------------Installing Kubectl, Kubelet and Kubeadm-------------"
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
@@ -60,22 +60,22 @@ sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
 
 echo "-------------Pulling Kueadm Images -------------"
-kubeadm config images pull
+sudo kubeadm config images pull
 #kubeadm config images list
 
 echo "-------------Running kubeadm init-------------"
-kubeadm init --pod-network-cidr=10.10.12.0/24
+sudo kubeadm init --pod-network-cidr=10.10.12.0/24
 
 echo "-------------Copying Kubeconfig-------------"
-mkdir -p /$HOME/.kube
-cp -iv /etc/kubernetes/admin.conf /$HOME/.kube/config
+sudo mkdir -p /$HOME/.kube
+sudo cp -iv /etc/kubernetes/admin.conf /$HOME/.kube/config
 sudo chown $(id -u):$(id -g) /$HOME/.kube/config
 
 echo "-------------Deploying Calico Pod Networking-------------"
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml -O
+sudo kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/tigera-operator.yaml
+sudo curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.0/manifests/custom-resources.yaml -O
 sudo sed -i 's/cidr: 192\.168\.0\.0\/16/cidr: 10.10.12.0\/24/g' custom-resources.yaml
-kubectl create -f custom-resources.yaml 
+sudo kubectl create -f custom-resources.yaml 
 
 
 
